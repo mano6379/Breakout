8a5ee9aaa500ea7280891ae2b614a1d4cf78d077
Marion Ano <meaola@gmail.com>
2014-03-21 14:58:12 -0500

    all blocks delete

--- Breakout/BlockView.h
+++ Breakout/BlockView.h
@@ -10,4 +10,5 @@
 
 @interface BlockView : UIView
 
+-(void) howBlocksAppear;
 @end
--- Breakout/ViewController.m
+++ Breakout/ViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by Marion Ano on 3/20/14.
 //  Copyright (c) 2014 Marion Ano. All rights reserved.
-//
+// // blog on UIKit Dynamics (Building UIKit Pong) http://blog.bignerdranch.com/3899-uikit-dynamics-and-ios-7-building-uikit-pong/
 
 #import "ViewController.h"
 #import "PaddleView.h"
@@ -22,7 +22,20 @@
     UIDynamicItemBehavior* paddleDynamicBehavior;
     UIDynamicItemBehavior* blockDynamicBehavior;
     IBOutlet BlockView *blockView;
+    //create an NSMutalable array of myBlock objects
+    NSMutableArray *myBlocks;
 }
+
+@property BlockView* myTestBlockOne;
+@property BlockView* myTestBlockTwo;
+@property BlockView* myTestBlockThree;
+@property BlockView* myTestBlockFour;
+@property BlockView* myTestBlockFive;
+@property BlockView* myTestBlockSix;
+@property BlockView* myTestBlockSeven;
+@property BlockView* myTestBlockEight;
+
+
 
 @end
 
@@ -33,9 +46,46 @@
     [super viewDidLoad];
     
     
+    //Creating blockView objects programatically
+    self.myTestBlockOne = [[BlockView alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
+    self.myTestBlockOne.backgroundColor = [UIColor lightGrayColor];
+    [self.view addSubview:self.myTestBlockOne];
+    
+    self.myTestBlockTwo = [[BlockView alloc] initWithFrame:CGRectMake(40, 0, 40, 40)];
+    self.myTestBlockTwo.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:self.myTestBlockTwo];
+    
+    self.myTestBlockThree = [[BlockView alloc] initWithFrame:CGRectMake(80, 0, 40, 40)];
+    self.myTestBlockThree.backgroundColor = [UIColor redColor];
+    [self.view addSubview:self.myTestBlockThree];
+    
+    self.myTestBlockFour = [[BlockView alloc] initWithFrame:CGRectMake(120, 0, 40, 40)];
+    self.myTestBlockFour.backgroundColor = [UIColor lightGrayColor];
+    [self.view addSubview:self.myTestBlockFour];
+    
+    self.myTestBlockFive = [[BlockView alloc] initWithFrame:CGRectMake(160, 0, 40, 40)];
+    self.myTestBlockFive.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:self.myTestBlockFive];
+    
+    self.myTestBlockSix = [[BlockView alloc] initWithFrame:CGRectMake(200, 0, 40, 40)];
+    self.myTestBlockSix.backgroundColor = [UIColor redColor];
+    [self.view addSubview:self.myTestBlockSix];
+    
+    self.myTestBlockSeven = [[BlockView alloc] initWithFrame:CGRectMake(240, 0, 40, 40)];
+    self.myTestBlockSeven.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:self.myTestBlockSeven];
+    
+    self.myTestBlockEight= [[BlockView alloc] initWithFrame:CGRectMake(280, 0, 40, 40)];
+    self.myTestBlockEight.backgroundColor = [UIColor redColor];
+    [self.view addSubview:self.myTestBlockEight];
+    
+    //putting all programmatic BlockView objects into an NSMutable Array
+    myBlocks = [[NSMutableArray alloc]initWithObjects:self.myTestBlockOne, self.myTestBlockTwo, self.myTestBlockThree, self.myTestBlockFour, self.myTestBlockFive, self.myTestBlockSix, self.myTestBlockSeven, self.myTestBlockEight,nil];
+    
+    
     dynamicAnimator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
-	pushBehavior = [[UIPushBehavior alloc]initWithItems:@[ballView] mode:UIPushBehaviorModeInstantaneous];
-    collisionBehavior = [[UICollisionBehavior alloc]initWithItems:@[ballView,paddleView, blockView]];
+	pushBehavior = [[UIPushBehavior alloc]initWithItems:@[ballView] mode:UIPushBehaviorModeContinuous]; //change to continuous from instantaneous
+    collisionBehavior = [[UICollisionBehavior alloc]initWithItems:@[ballView, paddleView, self.myTestBlockOne, self.myTestBlockTwo, self.myTestBlockThree, self.myTestBlockFour, self.myTestBlockFive, self.myTestBlockSix, self.myTestBlockSeven, self.myTestBlockEight]];
     ballDynamicBehavior = [[UIDynamicItemBehavior alloc]initWithItems:@[ballView]];
     paddleDynamicBehavior = [[UIDynamicItemBehavior alloc]initWithItems:@[paddleView]];
     blockDynamicBehavior = [[UIDynamicItemBehavior alloc]initWithItems:@[blockView]];
@@ -45,9 +95,6 @@
     pushBehavior.magnitude =  0.2;
     [dynamicAnimator addBehavior:pushBehavior];
     
-    
-    
-    
     collisionBehavior.collisionMode = UICollisionBehaviorModeEverything;
     collisionBehavior.translatesReferenceBoundsIntoBoundary = YES; //turn reference bounds into a boundary (UIView becomes of reference boundary)
     
@@ -55,7 +102,6 @@
     //self (I) (view controller) am talking to UICollision behavior, which has a property called CollionDelagate, and I am setting myself as that property.
     
     collisionBehavior.collisionDelegate = self;
-    
     [dynamicAnimator addBehavior:collisionBehavior];
     
     
@@ -69,6 +115,9 @@
     paddleDynamicBehavior.allowsRotation = NO;
     paddleDynamicBehavior.density = 1000;
     [dynamicAnimator addBehavior:paddleDynamicBehavior];
+    
+    //ballView.gravity = CGVectorMake(0.0f, 0.0f);
+    
 }
 
 - (IBAction)dragPaddle:(UIPanGestureRecognizer*)panGestureRecognizer
@@ -92,7 +141,25 @@
 
 -(void)collisionBehavior:(UICollisionBehavior *)behavior beganContactForItem:(id<UIDynamicItem>)item1 withItem:(id<UIDynamicItem>)item2 atPoint:(CGPoint)p
 {
-    [collisionBehavior removeItem:item2];
+ 
+    //for(myBlocks *myBlockViews in self.view.subviews)
+    //if ([item2 isKindOfClass:[BlockView class]])
+    
+    if ([item2 isMemberOfClass:[BlockView class]])
+ {
+     [collisionBehavior removeItem:item2];
+     [myBlocks removeObject:item2];
+     //you can only remove a view from itʻs Superview
+     [(BlockView*)item2 removeFromSuperview];
+     [dynamicAnimator updateItemUsingCurrentState:item2];
+     //[collisionBehavior re]
+ }
+    
+}
+
+-(void) addBlocksToView
+{
+    
 }
         //id item that is blockview
 //    if ([item1 isKindOfClass:[BlockView class]])
