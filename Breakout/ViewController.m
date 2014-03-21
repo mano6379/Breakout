e66b799b96bfd4acc1b2a09a950607736d276000
Marion Ano <meaola@gmail.com>
2014-03-20 19:20:43 -0500

    reset ball to center

--- Breakout/ViewController.m
+++ Breakout/ViewController.m
@@ -44,7 +44,7 @@
     
     
     collisionBehavior.collisionMode = UICollisionBehaviorModeEverything;
-    collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
+    collisionBehavior.translatesReferenceBoundsIntoBoundary = YES; //turn reference bounds into a boundary (UIView becomes of reference boundary)
     
     //the next line says that I (view controller) want to be your delegate and am indeed setting myself equal to you and your behavior
     //self (I) (view controller) am talking to UICollision behavior, which has a property called CollionDelagate, and I am setting myself as that property.
@@ -54,7 +54,7 @@
     [dynamicAnimator addBehavior:collisionBehavior];
     
     
-    ballDynamicBehavior.allowsRotation = YES;
+    ballDynamicBehavior.allowsRotation = NO;
     ballDynamicBehavior.elasticity = 1.0;
     ballDynamicBehavior.friction = 0; //since the default value is 0, we don't actually have to call this!
     [dynamicAnimator addBehavior:ballDynamicBehavior];
@@ -74,7 +74,12 @@
 
 -(void)collisionBehavior:(UICollisionBehavior *)behavior beganContactForItem:(id<UIDynamicItem>)item withBoundaryIdentifier:(id<NSCopying>)identifier atPoint:(CGPoint)p
 {
-       NSLog(@"X %f y %f", p.x, p.y);
+    //origin.y is the y coordinate relative to the main UIView
+    if (ballView.frame.origin.y >= (self.view.frame.size.height-(ballView.frame.size.height*2)))
+    {
+        ballView.center = self.view.center;
+        [dynamicAnimator updateItemUsingCurrentState:ballView];
+    }
         
 }
 
